420598f470455e2ce99acc6cf6f1e7dcaec18c03
JE668 <63463703+JE668@users.noreply.github.com>
2021-09-14 18:05:35 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,4 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
